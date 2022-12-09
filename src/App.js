c8d01884fd243fcb0a1158a27fe3ad10d11dcaea
Sanjaanorov <86147977+Sanjm3tr@users.noreply.github.com>
2022-12-09 18:44:49 +0800

    ok

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import { Box, Container } from "@mui/system";
+import { useState } from "react";
+import { Button, Grid } from "@mui/material";
+
+import { Images } from "./assets/imgCollection";
+import CardCover from "./assets/images/card-image.jpeg";
+
+const Card = ({ image, flip, isFlipped }) => {
+  return (
+    <Box
+      onClick={() => flip()}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        height: "200px",
+        position: "relative",
+        transform: `rotateY(${isFlipped ? "180deg" : "0deg"})`,
+        transformStyle: "preserve-3d",
+        transition: "transform 0.6s",
+        boxSizing: "border-box",
+        borderRadius: "15px",
+      }}
+    >
+      {image !== null && (
+        <>
+          <img
+            src={CardCover}
+            alt="cardcover"
+            style={{
+              position: "absolute",
+              height: "100%",
+              width: "200px",
+              backfaceVisibility: "hidden",
+            }}
+          />
+
+          <img
+            src={image.src}
+            alt="something"
+            style={{
+              position: "absolute",
+              height: "100%",
+              width: "200px",
+              backfaceVisibility: "hidden",
+              transform: "rotateY(180deg)",
+              borderRadius: "15px",
+            }}
+          />
+        </>
+      )}
+    </Box>
+  );
+};
+
+const CardContainer = () => {
+  const [images, setImage] = useState([]);
+  const [cardFlip, setCardFlip] = useState(false);
+  const [firstFlip, setFisrtFlip] = useState(null);
+
+  const [flipped, setFlipped] = useState(new Array(10).fill(false));
+
+  const generate = () => {
+    const newImages = [...Images, ...Images].sort(
+      (a, b) => Math.random() - 0.5
+    );
+    setImage(newImages);
+  };
+  const flip = (index) => {
+    let newFlipped = flipped.map((item, i) => (i === index ? !item : item));
+    // let flippedCount = newFlipped.filter((flip) => flip === true);
+    setFlipped(newFlipped);
+    if (firstFlip === null) {
+      setFisrtFlip(index);
+    } else {
+      if (images[firstFlip] === images[index] && index !== firstFlip) {
+        setTimeout(() => {
+          setImage(images.map((img) => (img === images[index] ? null : img)));
+        }, 600);
+      }
+      setTimeout(() => {
+        setFisrtFlip(null);
+        setFlipped(new Array(10).fill(false));
+      }, 600);
+    }
+  };
+
+  return (
+    <Box>
+      <Container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Grid spacing={3} container>
+          {images.map((image, index) => {
+            return (
+              <Grid item xl={2.4} md={2.4}>
+                <Card
+                  flip={() => flip(index)}
+                  image={image}
+                  isFlipped={flipped[index]}
+                  cardFlip={cardFlip}
+                  key={index}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+      <Button
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center  ",
+          backgroundColor: "red",
+          position: "fixed",
+        }}
+        onClick={() => generate()}
+      >
+        Hoho
+      </Button>
+    </Box>
+  );
+};
+
+export default CardContainer;
---
+++ src/assets/imgCollection.jsx
@@ -0,0 +1,22 @@
+export const Images = [
+  {
+    src: require("./images/chad.jpeg"),
+    id: 1,
+  },
+  {
+    src: require("./images/giga.jpeg"),
+    id: 2,
+  },
+  {
+    src: require("./images/wojak.jpeg"),
+    id: 3,
+  },
+  {
+    src: require("./images/wojak1.jpeg"),
+    id: 4,
+  },
+  {
+    src: require("./images/wojak2.jpeg"),
+    id: 5,
+  },
+];
